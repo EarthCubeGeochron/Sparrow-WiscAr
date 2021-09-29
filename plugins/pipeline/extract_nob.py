f13574a62a58e4de9326c0c2cfa0aedafbf0bdd7
Ben Bruck <bbruck@wisc.edu>
2021-09-29 13:12:11 -0500

    Testing commit

--- plugins/pipeline/extract_nob.py
+++ plugins/pipeline/extract_nob.py
@@ -26,18 +26,18 @@ def separate_samples(df):
     return sdf
 
 def sample_info(df):
-        # Get index for the top of the Sample table
-        ixa = value_index(df, "Sample:")
-        ixb = value_index(df, "Material:")
-        ixc = value_index(df, "Identifier:")
-        sample = (df.iloc[ixa[0]:ixb[0]+1,:]
-            .dropna(axis=0, how='all')
-            .dropna(axis=1, how='all'))
-        # Move the Identifier chunk down into the same column as Sample
-        sample.at[ixb[0]+1,ixb[1]] = sample.at[ixc]
-        sample.at[ixb[0]+1,ixb[1]+1] = sample.at[ixc[0],ixc[1]+1]
-        sample = sample.drop([ixc[1],ixc[1]+1], axis=1)
-        return(sample)
+    # Get index for the top of the Sample table
+    ixa = value_index(df, "Sample:")
+    ixb = value_index(df, "Material:")
+    ixc = value_index(df, "Identifier:")
+    sample = (df.iloc[ixa[0]:ixb[0]+1,:]
+        .dropna(axis=0, how='all')
+        .dropna(axis=1, how='all'))
+    # Move the Identifier chunk down into the same column as Sample
+    sample.at[ixb[0]+1,ixb[1]] = sample.at[ixc]
+    sample.at[ixb[0]+1,ixb[1]+1] = sample.at[ixc[0],ixc[1]+1]
+    sample = sample.drop([ixc[1],ixc[1]+1], axis=1)
+    return(sample)
 
 def extract_table(df):
     #get index of top of the results table
@@ -62,3 +62,15 @@ def extract_summary(df):
             .dropna(axis=0, how='all')
             .dropna(axis=1, how='all'))
     return summary
+
+def extract_nob_tables(df):
+    df = read_excel('//wsl$/Ubuntu-20.04/home/bbruck/Sparrow-WiscAr/Data/Noblesse-test-data/Complete data_Cooper_biotite_Aug2019.xlsx')
+    df.columns = range(df.columns.size)
+    #get index for column labels
+    ixcl = value_index(df, "N")
+    #create dataframe from column labels, and drop those from df
+    label = (df.iloc[ixcl[0],:])
+    label.iloc[0] = 'omit' 
+    df = df.drop([ixcl[0]-1,ixcl[0],ixcl[0]+1])
+
+    sample
