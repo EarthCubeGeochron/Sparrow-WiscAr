8667d59d13a8c7d7a9efd1b02175d4e1d0deb339
Ben Bruck <bbruck@wisc.edu>
2022-05-27 16:21:38 -0500

    Finished Noblesse pre-pychron importer. Still not 100% effective, but pulling in most files.

--- plugins/pipeline/extract_noblesse.py
+++ plugins/pipeline/extract_noblesse.py
@@ -1,10 +1,20 @@
-from pandas import read_excel, Series, concat
+from pandas import read_excel, Series
 import numpy as N
 
-def separate_samples(df):    
+def value_index(df, value, integer=False):
+    for i, row in df.iterrows():
+        for j, col in row.iteritems():
+            if col != value: continue
+            if integer:
+                i = df.index.get_loc(i)
+                j = df.columns.get_loc(j)
+            return (i,j)
+
+def separate_samples(df):
+                   
     #identify the instances of Sample: in the file
     ixs = list()
-    result = df.isin(["Sample:"])
+    result = df.isin(["Sample: "])
     series = result.any()
     columnNames = list(series[series == True].index)
     for col in columnNames:
@@ -25,57 +35,150 @@ def separate_samples(df):
                 .dropna(axis=1, how='all'))
     return sdf
 
-def sample_info(df):
-    # Get index for the top of the Sample table
-    ixa = value_index(df, "Sample:")
-    ixb = value_index(df, "Material:")
-    ixc = value_index(df, "Identifier:")
-    sample = (df.iloc[ixa[0]:ixb[0]+1,:]
-        .dropna(axis=0, how='all')
-        .dropna(axis=1, how='all'))
-    # Move the Identifier chunk down into the same column as Sample
-    sample.at[ixb[0]+1,ixb[1]] = sample.at[ixc]
-    sample.at[ixb[0]+1,ixb[1]+1] = sample.at[ixc[0],ixc[1]+1]
-    sample = sample.drop([ixc[1],ixc[1]+1], axis=1)
+def analysis_type(df):
+    a_label = df.iloc[1,0]
+    if "fusion" or "Fusion" in a_label:
+        a_type = 'Total Fusion Age'
+    else:
+        a_type = 'Age Plateau'
+    return(a_type)
+
+def sample_info(xdf):
+    sample = (xdf.iloc[0:2,:]
+            .dropna(axis=0, how='all')
+            .dropna(axis=1, how='all'))
+
+    #Remove space from sample:
+    sample.iloc[0,0] = 'Sample:'
+    
+    # Move the J value down into the same column as Sample
+    jv = sample.iloc[0:1,2:5]
+    jv.columns = [0,1,2]
+    sample = sample.iloc[:,0:2]
+    sample = sample.append(jv)
+    
+    for i in range(len(sample.iloc[:,0])):
+        sample.iloc[i,0] = sample.iloc[i,0].strip()
+    
+    #promote first column to index
+    sample = sample.set_index(0)
+    sample = sample.transpose()
+    
+        
     return(sample)
 
-def extract_table(df):
+def extract_table(xdf):
     #get index of top of the results table
-    ixb = value_index(df, "Material:")
-    ixd = value_index(df, "Integrated K/Ca ±2σ")
-    # Clean the table
-    results = (df.loc[ixb[0]+1:ixd[0]-1,0:]
+    results = (xdf.iloc[2:,:]
             .dropna(axis=0, how='all'))
-    results.set_axis([label], axis=1, inplace=True)        
-    return results
 
-def extract_summary(df):
-    #get index of top of the summary table
-    ixd = value_index(df, "Integrated K/Ca ±2σ")
-    try:
-        ixf = value_index(df, "Notes:")
-        summary = (df.loc[ixd[0]:ixf[0]-1,:]
-            .dropna(axis=0, how='all')
-            .dropna(axis=1, how='all'))
-    except:   
-        summary = (df.loc[ixd[0]:,:]
-            .dropna(axis=0, how='all')
-            .dropna(axis=1, how='all'))
-    return summary
-
-def extract_noblesse_tables(fn):
-    df = read_excel(fn)
-    df.columns = range(df.columns.size)
-    #get index for column labels
-    ixcl = value_index(df, "N")
-    #create dataframe from column labels, and drop those from df
-    label = (df.iloc[ixcl[0],:])
-    label.iloc[0] = 'omit' 
-    df = df.drop([ixcl[0]-1,ixcl[0],ixcl[0]+1])
-
+    #collapse the two rows of column labels into one
     
+    for i in range(len(results.iloc[0,:])):
+        if type(results.iloc[0,i]) != float:
+            results.iloc[1,i] = results.iloc[0,i] + ' ' + results.iloc[1,i]
+        if 'in' in results.iloc[1,i]:
+            results.iloc[1,i] = 'in_plateau'
 
-def test_read(fn):
-    df = read_excel(fn)
-    grab_cell = df.iloc[0,0]
-    return grab_cell
+    results = results.iloc[1:,:]
+    #find the end of the table and separate that off into its own df
+    et = results.iloc[:,0].isna()
+    et = list(et[et == True].index)
+
+    age = (results.loc[et[0]:et[0]+1,:]
+            .dropna(axis=0, how='all')
+            .dropna(axis=1, how='all'))
+
+
+    #trim that weighted mean age/plateau age off the results table
+    results = results.loc[:et[0]-1,:]
+
+    results.columns = results.iloc[0]
+    results = results.iloc[1:]
+    results.reset_index()
+
+    results['in_plateau'] = results['in_plateau'].notna()
+    # Tag age df with unit and error
+    if 'Ma' in results.columns:
+        age.iloc[0,0] = age.iloc[0,0] + ' Ma'
+    else:
+        age.iloc[0,0] = age.iloc[0,0] + ' ka'
+    
+    if '± 2σ (Ma)' or '± 2σ (ka)' in results.columns:
+        age.iloc[0,0] = age.iloc[0,0] + ' 2σ'
+    else:
+        age.iloc[0,0] = age.iloc[0,0] + ' 1σ'
+
+    return(results, age)
+
+def extract_notes(xdf):
+    # isolate notes section
+
+    # normal format starts with instrument
+    try:
+        ixn = value_index(xdf, "Instrument:")
+        notes = (df.loc[ixn[0]:ixn[0]+2,:]
+                .dropna(axis=0, how='all')
+                .dropna(axis=1, how='all'))
+        notes.columns = (0,1,2,3)
+
+    # Unusual format starts with J-value
+    except:
+        try:
+            ixn = value_index(xdf, "J-value:")
+            notes = (xdf.loc[ixn[0]:ixn[0]+1,:]
+                        .dropna(axis=0, how='all')
+                    .dropna(axis=1, how='all'))
+            notes.columns = (0,1,2,3)
+        except:
+            # If this isn't the last sample block in the file, it won't have a notes section
+            notes = "None"
+
+    #Fine the other three note sections
+    ixa = value_index(xdf, "Atmospheric argon ratios ")
+    ixb = value_index(xdf, "Interfering isotope production ratios")
+    ixc = value_index(xdf, "Decay constants ")
+
+    # Make sure these are present
+    if ixa != None:
+        
+        #Atmospheric argon ratios
+        n1 = (df.loc[ixa[0]+1:ixb[0]-1,ixa[1]:ixa[1]+4]
+                .dropna(axis=0, how='all')
+                .dropna(axis=1, how='all'))
+        n1.columns = (0,1,2,3)
+
+        #Interfering Isotope Production Ratios
+        try:    
+            n2 = (xdf.loc[ixb[0]+1:ixb[0]+5,ixb[1]:ixb[1]+4]
+                    .dropna(axis=0, how='all')
+                    .dropna(axis=1, how='all'))
+            n2.columns = (0,1,2,3)
+        except:
+            n2 = (xdf.loc[ixb[0]+1:ixb[0]+5,ixb[1]:ixb[1]+4]
+                    .dropna(axis=0, how='all')
+                    .dropna(axis=1, how='all'))
+            n2.columns = (0,1,2)
+
+        #Decay Constants
+        n3 = (xdf.loc[ixc[0]+1:,ixc[1]:ixc[1]+4]
+                .dropna(axis=0, how='all')
+                .dropna(axis=1, how='all'))
+        n3.columns=(0,1,2)
+
+        #Combine into one table
+        notes = notes.append(n1)
+        notes = notes.append(n2)
+        notes = notes.append(n3)
+
+        notes = notes.set_index(0)
+        notes = notes.transpose()
+       
+    return(notes)
+
+def extract_noblesse_tables(xdf):
+    info = sample_info(xdf)
+    data, age = extract_table(xdf)
+    notes = extract_notes(xdf)
+    return(info, data, age, notes)
+    
--- plugins/pipeline/importer.py
+++ plugins/pipeline/importer.py
@@ -3,12 +3,15 @@ from os import environ, listdir, path
 from datetime import datetime
 from click import command, option, echo, secho, style
 from pathlib import Path
+
+from pandas import read_excel
+#from plugins.pipeline.extract_noblesse import sample_info
 from sparrow.database import get_or_create
 from sparrow.import_helpers import BaseImporter, SparrowImportError
 from IPython import embed
 
 from .extract_tables import extract_data_tables
-from .extract_noblesse import separate_samples, extract_noblesse_tables, test_read
+from .extract_noblesse import separate_samples, extract_noblesse_tables, analysis_type
 
 def print_dataframe(df):
     secho(str(df.fillna(''))+'\n', dim=True)
@@ -17,15 +20,21 @@ param_data = {
     'Tstep': "Temperature of heating step",
     'power': "Laser power of heating step",
     '36Ar(a)': "36Ar, corrected for air interference",
+    '36Ar (cps)': "36Ar counts per second",
     '37Ar(ca)': "37Ar, corrected for Ca interference",
+    '37Ar (cps)': "37Ar counts per second",
     '38Ar(cl)': "38Ar, corrected for Cl interference",
     '39Ar(k)': "39Ar, corrected for amount produced by K",
+    '39Ar (cps)': "39Ar counts per second",
     '40Ar(r)': "Radiogenic 40Ar measured abundance",
+    '40Ar (cps)': "40Ar counts per second",
     '40(r)/39(k)': "Ratio of radiogenic 40Ar to 39Ar from K",
+    '40Ar*/39ArK': "Ratio of radiogenic 40Ar to 39Ar from K",
     'step_age': "Age calculated for a single heating step",
     'plateau_age': "Age calculated for a plateau",
     'total_fusion_age': "Age calculated for fusion of all heating steps",
     '40Ar(r) [%]': "Radiogenic 40Ar, percent released in this step",
+    '%40Ar*': "Radiogenic 40Ar, percent released in this step",
     '39Ar(k) [%]': "39Ar from potassium, percent released in this step",
     'K/Ca': "Potassium/Calcium ratio"
 }
@@ -39,61 +48,226 @@ def split_error(v):
 class NoblesseImporter(BaseImporter):
     authority = "WiscAr"
     file_type = "Noblesse"
+    def __init__(self, db, **kwargs):
+        self.show_data = kwargs.pop('show_data', False)
+        super().__init__(db, **kwargs)
+        self.create_parameters()
 
+    def create_parameters(self):
+        for k,v in param_data.items():
+            p = self.parameter(k, v)
+            self.add(p)
+        # Create error metrics
+        V = self.unit('V', 'Measured isotope abundance')
+        C = self.unit('cps', 'Counts per second')
+        em1 = self.error_metric('1s', '1 standard deviation')
+        em2 = self.error_metric('2s', '2 standard deviations')
+
+        at1 = self.analysis_type("Total Fusion Age", "Integrated Age")
+        at2 = self.analysis_type("Plateau Age", "Integrated Age")
+
+        # Set the description for the data file type
+        v = self.db.session.query(self.m.data_file_type).get(self.file_type)
+        v.description = 'Formatted Excel Spreadsheet'
+
+        self.add(v, V, C, em1, em2, at1, at2)
+    
     def import_datafile(self, fn, rec, **kwargs):
         """
         Import an original data file
         """
+
+        mod_time = datetime.fromtimestamp(path.getmtime(fn))
+        
+        df = read_excel(fn, header = None)
+        a_type = analysis_type(df)
+
+        sample_set = separate_samples(df)
+            
+        for i in range(len(sample_set)):
+            try:
+                info, data, age, notes = extract_noblesse_tables(sample_set[i])
+            except:
+                raise SparrowImportError(str())
+            
+            #if self.show_data:
+                #print_dataframe(data)
+                #print_dataframe(age)
+                #print_dataframe(info)
+                #print_dataframe(notes)
+            
+            
+            sn = info.pop('Sample:')
+            sample = self.sample(name=sn[1])
+            mat = info.pop('Material:') 
+            target = self.material(mat[1])
+            instrument = self.db.get_or_create(
+            self.m.instrument,
+            name="Noblesse 5-collector mass spectrometer")
+                        
+            method = self.method("Ar/Ar "+ a_type)
+            self.add(sample, target, instrument, method)
+            self.db.session.flush()
+
+            session = self.db.get_or_create(
+                self.m.session,
+                sample_id=sample.id,
+                instrument=instrument.id,
+                technique=method.id,
+                date=mod_time,
+                target=target.id)
+            session.date_precision = "day"
+            self.add(session)
+            self.db.session.flush()
+
+
+            info = self.general_info(session, info, notes)
+            session.data = info.to_dict()    
+        
+            for i, step in enumerate(data.iterrows()):
+                self.import_heating_step(i, step, session, data)
+
+            self.import_age(session, a_type, age)    
+
+                # This function returns the top-level
+                # record that should be linked to the datafile
+            self.db.session.flush()
+            yield session   
+        
+            for i, step in enumerate(data.iterrows()):
+                self.import_heating_step(i, step, session, data)
+
+            self.import_age(session, a_type, age)    
+
+            # This function returns the top-level
+            # record that should be linked to the datafile
+            self.db.session.flush()
+            yield session
+    
+    def general_info(self, session, info, notes):
+        analysis = self.add_analysis(session, "General information")
+        
+        # J-value
         try:
-            sample_set, table_labels = separate_samples(fn)
-            for i in range(len(sample_set)):
-                try:
-                    info, data, results = extract_noblesse_tables(sample_set[i])
-                except:
-                    print("Noblesse Importer Error")
-                try:
-                    data.columns = table_labels
-                except:
-                    pass
-                if self.show_data:
-                    print_dataframe(info)
-                    print_dataframe(data)
-                    print_dataframe(results)
-                
-                # Set Instrument
-                instrument = self.db.get_or_create(
-                    self.m.instrument,
-                    name="Nu Instruments Noblesse")
-
-                # Get Sample Info
-                sample = self.sample(name=info.pop('Sample'))
-                target = self.material(info.pop('Material'))
-                identifier = info.pop('Identifier')
-                irradiation = identifier[1][0:5]
-                identifier = self.identifier(name=identifier)
-                irradiation = self.irradiation(name=irradiation)
-
-                # Set Session Info
-                session = self.db.get_or_create(
-                    self.m.session,
-                    sample_id=sample.id,
-                    instrument=instrument.id,
-                    technique=method.id,
-                    date=mod_time,
-                    target=target.id)
-                session.date_precision = "day"
-                self.add(session)
-                self.db.session.flush()
-                
-                # Get Data 
-                 
-                
+            value, error = info.pop('J-value:')
+            self.datum(analysis, "J-value", value, error=error)
+        except:
+            value, error = notes.pop('J-value:')
+            self.datum(analysis, "J-value", value, error=error)
+      
+        # FC or AC tuff age
+        try:
+            std = notes.pop('Standard:')
+            value, error = notes.pop('Standard age (Ma):')
+            self.constant(analysis, std+" standard age",
+                    value,
+                    error=error,
+                    unit='Ma')
         except:
             pass
+               
+        self.db.session.flush()
+        return info
+    
+    def import_heating_step(self, i, step, session, data):
+        (ix, row) = step
 
-        
+        analysis = self.add_analysis(session, "Heating step",
+            analysis_name= row['File'],
+            session_index=i)
+        analysis.in_plateau = row['in_plateau']
+        analysis.is_interpreted = False
+        self.add(analysis)
 
+        try:
+            self.datum(analysis, 'power', row['Laser Power'], unit='%',
+                    description='Laser power for heating step')
+
+        except:
+            pass
             
+        for param in ['40Ar (cps)','39Ar (cps)','37Ar (cps)','36Ar (cps)',
+                  '40Ar*/39ArK','%40Ar*']:
+            unit = 'cps'
+            if '*' in param:
+                unit = 'V'
+            if '/' in param:
+                unit = 'ratio'
+            self.datum(analysis, param, row[param],
+                unit=unit, description=param_data[param])
+
+        try:
+            ix = list(row.index).index('Age (Ma)')
+            error_ix = ix+1
+            em = self.get_error_metric(row.index[error_ix])
+            self.datum(analysis, 'step_age', row['Age (Ma)'],
+                unit='Ma',
+                error=row.iloc[error_ix],
+                error_metric=em,
+                error_unit='Ma')
+            age_unit = 'Ma'
+        except:
+            ix = list(row.index).index('Age (ka)')
+            error_ix = ix+1
+            em = self.get_error_metric(row.index[error_ix])
+            self.datum(analysis, 'step_age', row['Age (ka)'],
+                unit='Ma',
+                error=row.iloc[error_ix],
+                error_metric=em,
+                error_unit='Ma')
+            age_unit = 'ka'
+
+        self.add_K_Ca_ratio(analysis, row)
+        self.db.session.flush()
+        return age_unit
+
+    def get_error_metric(self, label):
+        return label.replace("± ","")
+
+    def add_K_Ca_ratio(self, analysis, row):
+        ix = list(row.index).index('K/Ca')
+        self.datum(analysis, 'K/Ca', row['K/Ca'],
+            unit='ratio')
+
+    def import_age(self, session, a_type, age, **kwargs):
+        if a_type == 'Total Fusion Age':
+            age_parameter = 'total_fusion_age'
+        
+            analysis = self.analysis(
+                session_id = session.id,
+                is_interpreted=True,
+                type='Total Fusion Age')
+        else:
+            age_parameter = 'plateau_age'
+        
+            analysis = self.analysis(
+                session_id = session.id,
+                is_interpreted=True,
+                type='Age Plateau')
+                
+        ax = age.iloc[0,1]
+        ae = age.iloc[0,2]
+
+        if 'Ma' in age.iloc[0,0]:
+            age_unit = 'Ma'
+        else:
+            age_unit = 'ka'
+        
+        if '2σ' in age.iloc[0,0]:
+            em = '2σ'
+        
+        
+        #row = self.import_shared_parameters(analysis, row)
+        self.datum(analysis,
+            value=ax,
+            unit=age_unit,
+            parameter=age_parameter,
+            error=ae,
+            error_metric=em,
+            is_computed=True,
+            error_unit=age_unit,
+            **kwargs)
+
 
 class MAPImporter(BaseImporter):
     authority = "WiscAr"
